6efecc11971e175dc7abfb9a8c7f5a3ab0cd22b0
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2020-12-30 19:20:22 +0530

    Revert "add get-element module"
    This reverts commit 1c752aa5758ed13323d3cc5f705723e450beb107.

--- 2-random-user/utilities/get-element.js
+++
@@ -1,9 +0,0 @@
-//function to select an element
-
-const getElement = (selection) => {
-    const element = document.querySelector(selection);
-    if(element) return element;
-    throw new Error (`no such ${selection} element`);
-} 
-
-export default getElement;
