5e55b0b3eb7f2b58bf42c3e75da29dfdb1e67b25
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2020-12-30 19:55:52 +0530

    add displayUser module

--- 2-random-user/app.js
+++ 2-random-user/app.js
@@ -1,24 +1,19 @@
 import get from "./utilities/get-element.js";
 import getUser from './utilities/getUser.js';
+import displayUser from './utilities/displayUser.js';
 
 
-const img = get('.user-img');
-const title = get('.user-title');
-const value = get('.user-value');
+
 const btn = get('.btn');
-
-const btns = [...document.querySelectorAll('.icon')];
-
 
 const showUser = async () => {
     const person  = await getUser();
-    console.log(person);
+    displayUser(person);
+};
 
-}
+btn.addEventListener('click', showUser);
 
-btn.addEventListener('click', ()=>{
-    showUser();
-});
+window.addEventListener('DOMContentLoaded', showUser);
 
 
 
